bb891b6b5b839740dc36023c2b2c7a51566060f6
joseph rios <blankworldwide@gmail.com>
2019-01-08 03:59:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+  } 
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+}
